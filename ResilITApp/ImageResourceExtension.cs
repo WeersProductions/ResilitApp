5171142f501eb224d14f7cec478c60444607b5d9
Floris <weersproductions@gmail.com>
2019-08-03 18:52:05 +0200

    Add logo

---
+++ ResilITApp/ImageResourceExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ResilITApp
+{
+    [ContentProperty (nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            //var assembly = typeof(EmbeddedImages).GetTypeInfo().Assembly;
+            //foreach(var res in assembly.GetManifestResourceNames())
+            //{
+            //    System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            //}
+
+            if(Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require.
+            //var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            var imageSource = ImageSource.FromResource(Source);
+
+            return imageSource;
+        }
+    }
+}
