70408d8505e084297c2f1ce44f7f5206445a582b
Floris Weers <f.weers@kpnmail.nl>
2019-08-03 15:22:43 +0200

    Initial commit

---
+++ ResilITApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using System.Reflection;
+
+using Syncfusion.SfDataGrid.XForms.UWP;
+
+using Syncfusion.SfPullToRefresh.XForms.UWP;
+
+using Syncfusion.XForms.UWP.PopupLayout;
+
+namespace ResilITApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+			SfPopupLayoutRenderer.Init();
+
+			SfPullToRefreshRenderer.Init();
+
+			SfDataGridRenderer.Init();
+
+            LoadApplication(new ResilITApp.App());
+        }
+    }
+}
---
+++ ResilITApp.iOS/AppDelegate.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+using Syncfusion.SfDataGrid.XForms.iOS;
+
+using Syncfusion.SfPdfViewer.XForms.iOS; 
+
+using Syncfusion.SfRangeSlider.XForms.iOS; 
+
+using Syncfusion.SfSchedule.XForms.iOS;
+
+using Syncfusion.XForms.iOS.ProgressBar; 
+
+using Syncfusion.SfCalendar.XForms.iOS;
+
+using Syncfusion.SfBusyIndicator.XForms.iOS;
+
+using Syncfusion.SfNavigationDrawer.XForms.iOS;
+
+using Syncfusion.SfNumericTextBox.XForms.iOS;
+
+using Syncfusion.SfNumericUpDown.XForms.iOS;
+
+using Syncfusion.SfRadialMenu.XForms.iOS;
+
+using Syncfusion.SfPullToRefresh.XForms.iOS;
+
+using Syncfusion.XForms.iOS.DataForm;
+
+using Syncfusion.XForms.iOS.PopupLayout;
+
+using Syncfusion.XForms.iOS.TabView;
+
+using Syncfusion.XForms.iOS.Buttons;
+
+using Syncfusion.XForms.iOS.Accordion;
+
+namespace ResilITApp.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            
+			SfDataGridRenderer.Init();
+			
+			
+			SfPdfDocumentViewRenderer.Init(); 
+			
+			
+			SfRangeSliderRenderer.Init(); 
+			
+			
+			SfScheduleRenderer.Init();
+			
+			
+			SfLinearProgressBarRenderer.Init(); 
+			
+			
+			SfCircularProgressBarRenderer.Init(); 
+			
+			
+			SfCalendarRenderer.Init();
+			
+			
+			SfBusyIndicatorRenderer.Init();
+			
+			
+			SfNavigationDrawerRenderer.Init();
+			
+			
+			SfNumericTextBoxRenderer.Init();
+			
+			
+			SfNumericUpDownRenderer.Init();
+			
+			
+			SfRadialMenuRenderer.Init();
+			
+			
+			SfPullToRefreshRenderer.Init();
+			
+			
+			SfDataFormRenderer.Init();
+			
+			
+			SfPopupLayoutRenderer.Init();
+			
+			
+			SfTabViewRenderer.Init();
+			
+			
+			SfCheckBoxRenderer.Init();
+			
+			
+			SfButtonRenderer.Init();
+			
+			
+			SfAccordionRenderer.Init();
+			
+			LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
