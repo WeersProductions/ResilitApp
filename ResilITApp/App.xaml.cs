afe4a65fbb951cce0437eae39d350c1d552960bb
Floris <weersproductions@gmail.com>
2019-08-04 12:45:10 +0200

    Add config.json

--- ResilITApp/App.xaml.cs
+++ ResilITApp/App.xaml.cs
@@ -1,19 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace ResilITApp
 {
     public partial class App : Application
     {
+        private const string ConfigFile = "config.json";
         public App()
         {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream($"{ assembly.GetName().Name}.{ ConfigFile}");
+            Config config = null;
+            using(var reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                config = JsonConvert.DeserializeObject<Config>(json);
+            }
+
+            if(config != null)
+            {
+                Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(config.SyncFusionKey);
+            }
+            
             InitializeComponent();
 
             MainPage = new ResilITApp.MainPage();
+            if (config == null)
+            {
+                MainPage.DisplayAlert("Error", "Could not find config.json. Please report this bug.", "OK");
+            }
         }
 
         protected override void OnStart()
